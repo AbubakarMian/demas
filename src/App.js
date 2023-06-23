c45bd9f321602858dcd1134b4d993d34ae63f1e8
AmeerMaavia <65680588+AmeerMaavia@users.noreply.github.com>
2023-06-23 17:46:35 +0500

    initial commit again

---
+++ src/App.js
@@ -0,0 +1,114 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Nav, Navbar, Container } from 'react-bootstrap'
+import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import Login from './js/login_page';
+import Home from './home';
+import About from './about';
+import Users from './users';
+import Starting from './starting';
+import Button from 'react-bootstrap/Button';
+// import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faHouse } from '@fortawesome/free-solid-svg-icons'
+import { faAddressCard } from '@fortawesome/free-solid-svg-icons'
+import { faUsers } from '@fortawesome/free-solid-svg-icons'
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Container fluid>
+          <Row>
+            <Offcanvas_colapsable_nav />
+          </Row>
+        </Container>
+        <Routes>
+          <Route Component={Login} path='/js/login'></Route>
+          <Route Component={Starting} path='/starting'></Route>
+          <Route Component={About} path='/about'></Route>
+          <Route Component={Users} path='/users'></Route>
+          <Route Component={Home} path='/'></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
+
+
+const Offcanvas_colapsable_nav = () => {
+  return (
+    <>
+      {/* {[false, 'sm', 'lg', 'lg', 'xl', 'xxl'].map((expand) => ( */}
+      <Navbar key='xl' expand='lg' className="bg-body-tertiary" variant="dark">
+        <Container fluid className="nav_back">
+          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-lg`} />
+          <Navbar.Brand href="#">De Mass</Navbar.Brand>
+          <Navbar.Offcanvas
+            id={`offcanvasNavbar-expand-lg`}
+            aria-labelledby={`offcanvasNavbarLabel-expand-lg`}
+            placement="start"
+          >
+            <Offcanvas.Header>
+              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-lg`}>
+                <div>
+                <img className="collapse_logo" src="./images/1.png"></img>
+
+                </div>
+                <div>
+
+                </div>
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                <div className='nav_bottom'><Nav.Link href="/"><FontAwesomeIcon icon={faHouse} /> Home</Nav.Link></div>
+                <div className='nav_bottom'><Nav.Link href="/about"><FontAwesomeIcon icon={faAddressCard} /> About</Nav.Link></div>
+                <div className='nav_bottom'><Nav.Link href="users"><FontAwesomeIcon icon={faUsers} /> Users</Nav.Link></div>
+                <div className='nav_bottom'><Nav.Link href="starting"><FontAwesomeIcon icon={faUsers} /> starting page</Nav.Link></div>
+                <div className='nav_bottom'><Nav.Link href="js/login"><FontAwesomeIcon icon={faUsers} /> login</Nav.Link></div>
+                <NavDropdown
+                  title="Dropdown"
+                  id={`offcanvasNavbarDropdown-expand-lg`}
+                >
+                  <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action5">
+                    Something else here
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+              <Form className="d-flex">
+                <Form.Control
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button className='search_btn' variant="outline-success"><FontAwesomeIcon icon={faMagnifyingGlass} /></Button>
+              </Form>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+      {/* ))} */}
+    </>
+  );
+}
---
+++ src/js/login_page.js
@@ -0,0 +1,82 @@
+import React from "react";
+import Carousel from 'react-bootstrap/Carousel';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+// import './styles/login_page.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRightArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Modal from 'react-bootstrap/Modal';
+import { useState } from 'react';
+
+
+
+
+
+
+export default function home_page_style() {
+    return (
+      <div>asdasdasdsa
+        {/* <Row>
+          <Home_crousel />
+        </Row>
+        <Container fluid>
+          <Row>
+            <Col><SingleTrip_buttons /></Col>
+            <Col><Packages_button /></Col>
+          </Row>
+          <Row>
+            <Col md={1}></Col>
+            <Col md={10}><h3 className="headin">PICKUP</h3></Col>
+            <Col md={1}></Col>
+          </Row>
+          <Row>
+            <Col md={1}></Col>
+            <Col md={10}><Input_area_pick /></Col>
+            <Col md={1}></Col>
+          </Row>
+          <Row>
+  
+            <Col md={1}></Col>
+            <Col md={10}><h3 className="headin">DROP OFF</h3></Col>
+            <Col md={1}></Col>
+          </Row>
+          <Row>
+            <Col md={1}></Col>
+            <Col md={10}><Input_area_drop /></Col>
+            <Col md={1}></Col>
+          </Row>
+          <Row>
+            <Col md={1}></Col>
+            <Col md={10}><h3 className="headin">PICKUP DATE & TIME</h3></Col>
+            <Col md={1}></Col>
+          </Row>
+          <Row>
+            <Col md={1}></Col>
+            <Col md={10}><Input_area_time /></Col>
+            <Col md={1}></Col>
+          </Row>
+          <Row>
+            <Col md={1}></Col>
+            <Col md={10}><h5 className="comment">Comments</h5></Col>
+            <Col md={1}></Col>
+          </Row>
+          <Row>
+            <Col md={1}></Col>
+            <Col md={10}><Proceed_buttons /></Col>
+            <Col md={1}></Col>
+          </Row>
+  
+        </Container> */}
+      </div>
+    );
+  }
+  
