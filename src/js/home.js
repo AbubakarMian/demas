1c775abc6fb22f41490d75b24a41060951ee261f
AmeerMaavia <65680588+AmeerMaavia@users.noreply.github.com>
2023-07-21 13:54:21 +0500

    fixes now

--- src/js/home.js
+++ src/js/home.js
@@ -1,96 +1,242 @@
 import React from "react";
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Offcanvas from 'react-bootstrap/Offcanvas';
-import './../styles/home.css';
-import Carousel from 'react-bootstrap/Carousel';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRightArrowLeft } from '@fortawesome/free-solid-svg-icons'
-import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
-import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Modal from 'react-bootstrap/Modal';
-import { useState } from 'react';
-import Collapse from 'react-bootstrap/Collapse';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Nav_bar_area from './NavBar';
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import "./../styles/home.css";
+import Carousel from "react-bootstrap/Carousel";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRightArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Modal from "react-bootstrap/Modal";
+import { useState } from "react";
+import Collapse from "react-bootstrap/Collapse";
+import InputGroup from "react-bootstrap/InputGroup";
+import Nav_bar_area from "./NavBar";
 
+export default function Package_style() {
+  const [show, setShow] = useState(false);
 
-
-
-
-export default function home_page_style() {
   return (
     <div>
-      <Nav_bar_area/>
-        <Home_crousel />
+      <Nav_bar_area />
+      <Home_crousel />
       <Container fluid>
         <Row>
-          <Col><SingleTrip_buttons /></Col>
-          <Col><Packages_button /></Col>
+          <Col>
+            <Button variant="primary" className="singtripbtn">
+              Single Trip{" "}
+              <FontAwesomeIcon className="icon_btn" icon={faLocationDot} beat />
+            </Button>
+          </Col>
+          <Col>
+            <Button href="./packages" variant="primary" className="singtripbtn">
+              Packages{" "}
+              <FontAwesomeIcon
+                className="icon_btn"
+                icon={faArrowRightArrowLeft}
+              />
+            </Button>
+          </Col>
         </Row>
         <Row>
           <Col md={1}></Col>
-          <Col md={10}><h3 className="headin">PICKUP</h3></Col>
-          <Col md={1}></Col>
-        </Row>
-        <Row>
-          <Col md={1}></Col>
-          <Col md={10}><Input_area_pick /></Col>
-          <Col md={1}></Col>
-        </Row>
-        <Row>
-
-          <Col md={1}></Col>
-          <Col md={10}><h3 className="headin">DROP OFF</h3></Col>
-          <Col md={1}></Col>
-        </Row>
-        <Row>
-          <Col md={1}></Col>
-          <Col md={10}><Input_area_drop /></Col>
-          <Col md={1}></Col>
-        </Row>
-        <Row>
-          <Col md={1}></Col>
-          <Col md={10}><h3 className="headin">PICKUP DATE & TIME</h3></Col>
+          <Col md={10}>
+            <h3 className="headin">PICKUP</h3>
+          </Col>
           <Col md={1}></Col>
         </Row>
         <Row>
           <Col md={1}></Col>
-          <Col md={10}><Input_area_time /></Col>
+          <Col md={10}>
+            
+      <Button className="pic_btn_modal" onClick={() => setShow(true)}>
+        <FontAwesomeIcon className="icon_btn" icon={faLocationDot} /> Select
+        PickUp Location{" "}
+        <div className="caret_down">
+          <FontAwesomeIcon icon={faAngleDown} />
+        </div>
+      </Button>
+      <Modal
+        show={show}
+        onHide={() => setShow(false)}
+        dialogClassName="modal-90w"
+        aria-labelledby="example-custom-modal-styling-title"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="example-custom-modal-styling-title">
+            Select Pickup location
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form className="asdasd">
+            {["radio"].map((type) => (
+              <div key={`reverse-${type}`} className="mb-3">
+                <Form.Check
+                  label="Madina Hotel"
+                  name="group1"
+                  type={type}
+                  id={`reverse-${type}-1`}
+                />
+                <Form.Check
+                  label="Madina AirPort"
+                  name="group1"
+                  type={type}
+                  id={`reverse-${type}-2`}
+                />
+                <Form.Check
+                  label="Jaddah AirPort"
+                  name="group1"
+                  type={type}
+                  id={`reverse-${type}-3`}
+                />{" "}
+                <Form.Check
+                  label="Jaddah Hotel"
+                  name="group1"
+                  type={type}
+                  id={`reverse-${type}-4`}
+                />
+                <Form.Check
+                  label="Makkah Hotel"
+                  name="group1"
+                  type={type}
+                  id={`reverse-${type}-5`}
+                />
+                <Form.Check
+                  label="Train Station"
+                  name="group1"
+                  type={type}
+                  id={`reverse-${type}-6`}
+                />
+              </div>
+            ))}
+          </Form>
+        </Modal.Body>
+      </Modal>
+          </Col>
           <Col md={1}></Col>
         </Row>
         <Row>
           <Col md={1}></Col>
-          <Col md={10}><Comment/></Col>
+          <Col md={10}>
+            <h3 className="headin">DROP OFF</h3>
+          </Col>
           <Col md={1}></Col>
         </Row>
         <Row>
           <Col md={1}></Col>
-          <Col md={10}><Proceed_buttons /></Col>
+          <Col md={10}>
+            
+      <Button className="pic_btn_modal" onClick={() => setShow(true)}>
+        <FontAwesomeIcon className="icon_btn" icon={faLocationDot} /> Select
+        Drop Off Location
+        <div className="caret_down">
+          <FontAwesomeIcon icon={faAngleDown} />
+        </div>
+      </Button>
+      <Modal
+        show={show}
+        onHide={() => setShow(false)}
+        dialogClassName="modal-90w"
+        aria-labelledby="example-custom-modal-styling-title"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="example-custom-modal-styling-title">
+            Select Drop Off Location
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form className="asdasd">
+            {["radio"].map((type) => (
+              <div key={`reverse-${type}`} className="mb-3">
+                <Form.Check
+                  label="Madina Hotel"
+                  name="group1"
+                  type={type}
+                  id={`reverse-${type}-1`}
+                />
+                <Form.Check
+                  label="Madina AirPort"
+                  name="group1"
+                  type={type}
+                  id={`reverse-${type}-2`}
+                />
+                <Form.Check
+                  label="Jaddah AirPort"
+                  name="group1"
+                  type={type}
+                  id={`reverse-${type}-3`}
+                />{" "}
+                <Form.Check
+                  label="Jaddah Hotel"
+                  name="group1"
+                  type={type}
+                  id={`reverse-${type}-4`}
+                />
+                <Form.Check
+                  label="Makkah Hotel"
+                  name="group1"
+                  type={type}
+                  id={`reverse-${type}-5`}
+                />
+                <Form.Check
+                  label="Train Station"
+                  name="group1"
+                  type={type}
+                  id={`reverse-${type}-6`}
+                />
+              </div>
+            ))}
+          </Form>
+        </Modal.Body>
+      </Modal>
+          </Col>
           <Col md={1}></Col>
         </Row>
-
+        <Row>
+          <Col md={1}></Col>
+          <Col md={10}>
+            <h3 className="headin">PICKUP DATE & TIME</h3>
+          </Col>
+          <Col md={1}></Col>
+        </Row>
+        <Row>
+          <Col md={1}></Col>
+          <Col md={10}>
+            <Input_area_time />
+          </Col>
+          <Col md={1}></Col>
+        </Row>
+        <Row>
+          <Col md={1}></Col>
+          <Col md={10}>
+            <Comment />
+          </Col>
+          <Col md={1}></Col>
+        </Row>
+        <Row>
+          <Col md={1}></Col>
+          <Col md={10}>
+            <Proceed_buttons />
+          </Col>
+          <Col md={1}></Col>
+        </Row>
       </Container>
     </div>
   );
 }
 
-
 const Home_crousel = () => {
   return (
     <Carousel>
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="./images/1.jpg"
-          alt="First slide"
-        />
+        <img className="d-block w-100" src="./images/1.jpg" alt="First slide" />
         <Carousel.Caption>
           {/* <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
@@ -109,11 +255,7 @@ const Home_crousel = () => {
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="./images/5.jpg"
-          alt="Third slide"
-        />
+        <img className="d-block w-100" src="./images/5.jpg" alt="Third slide" />
 
         <Carousel.Caption>
           {/* <h3>Third slide label</h3>
@@ -123,11 +265,7 @@ const Home_crousel = () => {
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="./images/6.jpg"
-          alt="Third slide"
-        />
+        <img className="d-block w-100" src="./images/6.jpg" alt="Third slide" />
 
         <Carousel.Caption>
           {/* <h3>Fourth slide label</h3>
@@ -137,11 +275,7 @@ const Home_crousel = () => {
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="./images/7.jpg"
-          alt="Third slide"
-        />
+        <img className="d-block w-100" src="./images/7.jpg" alt="Third slide" />
 
         <Carousel.Caption>
           {/* <h3>Fifth slide label</h3>
@@ -151,11 +285,7 @@ const Home_crousel = () => {
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="./images/8.jpg"
-          alt="Third slide"
-        />
+        <img className="d-block w-100" src="./images/8.jpg" alt="Third slide" />
 
         <Carousel.Caption>
           {/* <h3>Sixth slide label</h3>
@@ -164,173 +294,22 @@ const Home_crousel = () => {
           </p> */}
         </Carousel.Caption>
       </Carousel.Item>
-
     </Carousel>
-
-
   );
-}
-const SingleTrip_buttons = () => {
-  return (
-
-    <Button variant="primary" className="singtripbtn">Single Trip    <FontAwesomeIcon className="icon_btn" icon={faLocationDot} beat /></Button>
-  );
-}
-const Packages_button = () => {
-  return (
-
-    <Button  href="./packages" variant="primary" className="singtripbtn">Packages   <FontAwesomeIcon className="icon_btn" icon={faArrowRightArrowLeft} /></Button>
-  );
-}
-
+};
 
 const Input_area_pick = () => {
   const [show, setShow] = useState(false);
   return (
-
     <>
-
-      <Button className="pic_btn_modal" onClick={() => setShow(true)}>
-      <FontAwesomeIcon className="icon_btn" icon={faLocationDot}  /> Select PickUp Location <div className="caret_down"><FontAwesomeIcon  icon={faAngleDown} /></div>
-      </Button>
-      <Modal
-        show={show}
-        onHide={() => setShow(false)}
-        dialogClassName="modal-90w"
-        aria-labelledby="example-custom-modal-styling-title"
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="example-custom-modal-styling-title">
-          Select Pickup location
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-        <Form className="asdasd">
-      {[ 'radio'].map((type) => (
-        <div key={`reverse-${type}`} className="mb-3">
-          <Form.Check
-            
-            label="Madina Hotel"
-            name="group1"
-            type={type}
-            id={`reverse-${type}-1`}
-          />
-          <Form.Check
-            
-            label="Madina AirPort"
-            name="group1"
-            type={type}
-            id={`reverse-${type}-2`}
-          /><Form.Check
-            
-            label="Jaddah AirPort"
-            name="group1"
-            type={type}
-            id={`reverse-${type}-3`}
-          /> <Form.Check
-            
-            label="Jaddah Hotel"
-            name="group1"
-            type={type}
-            id={`reverse-${type}-4`}
-          />
-          <Form.Check
-            
-            label="Makkah Hotel"
-            name="group1"
-            type={type}
-            id={`reverse-${type}-5`}
-          /><Form.Check
-            
-            label="Train Station"
-            name="group1"
-            type={type}
-            id={`reverse-${type}-6`}
-          />
-          
-        </div>
-      ))}
-    </Form>
-        </Modal.Body>
-      </Modal>
-
-
     </>
   );
-}
-
+};
 
 const Input_area_drop = () => {
   const [show, setShow] = useState(false);
   return (
-
     <>
-
-      <Button className="pic_btn_modal" onClick={() => setShow(true)}>
-      <FontAwesomeIcon className="icon_btn" icon={faLocationDot}  /> Select Drop Off Location<div className="caret_down"><FontAwesomeIcon  icon={faAngleDown} /></div>
-      </Button>
-      <Modal
-        show={show}
-        onHide={() => setShow(false)}
-        dialogClassName="modal-90w"
-        aria-labelledby="example-custom-modal-styling-title"
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="example-custom-modal-styling-title">
-          Select Drop Off Location
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-        <Form className="asdasd">
-      {[ 'radio'].map((type) => (
-        <div key={`reverse-${type}`} className="mb-3">
-          <Form.Check
-            
-            label="Madina Hotel"
-            name="group1"
-            type={type}
-            id={`reverse-${type}-1`}
-          />
-          <Form.Check
-            
-            label="Madina AirPort"
-            name="group1"
-            type={type}
-            id={`reverse-${type}-2`}
-          /><Form.Check
-            
-            label="Jaddah AirPort"
-            name="group1"
-            type={type}
-            id={`reverse-${type}-3`}
-          /> <Form.Check
-            
-            label="Jaddah Hotel"
-            name="group1"
-            type={type}
-            id={`reverse-${type}-4`}
-          />
-          <Form.Check
-            
-            label="Makkah Hotel"
-            name="group1"
-            type={type}
-            id={`reverse-${type}-5`}
-          /><Form.Check
-            
-            label="Train Station"
-            name="group1"
-            type={type}
-            id={`reverse-${type}-6`}
-          />
-          
-        </div>
-      ))}
-    </Form>
-        </Modal.Body>
-      </Modal>
-
-
     </>
   );
   // return (
@@ -348,7 +327,7 @@ const Input_area_drop = () => {
   //     </Form.Text> */}
   //   </>
   // );
-}
+};
 
 const Input_area_time = () => {
   return (
@@ -366,41 +345,36 @@ const Input_area_time = () => {
       </Form.Text> */}
     </>
   );
-}
-
+};
 
 const Proceed_buttons = () => {
   return (
-
-    <Button variant="primary" className="Proceed_button">PROCEED TO NEXT</Button>
+    <Button variant="primary" className="Proceed_button">
+      PROCEED TO NEXT
+    </Button>
   );
-}
-
+};
 
 const Comment = () => {
-    const [open, setOpen] = useState(false);
-  
-    return (
-      <>
-        <Button
-          onClick={() => setOpen(!open)}
-          aria-controls="example-collapse-text"
-          aria-expanded={open}
-          className="commentsbtn"
-        >
-          Comment
-        </Button>
-        <Collapse in={open}>
-          <div id="example-collapse-text">
-          <InputGroup>
-        <Form.Control as="textarea" aria-label="With textarea" />
-      </InputGroup>
-          </div>
-        </Collapse>
-      </>
-    );
-  
-  
-  
-}
+  const [open, setOpen] = useState(false);
 
+  return (
+    <>
+      <Button
+        onClick={() => setOpen(!open)}
+        aria-controls="example-collapse-text"
+        aria-expanded={open}
+        className="commentsbtn"
+      >
+        Comment
+      </Button>
+      <Collapse in={open}>
+        <div id="example-collapse-text">
+          <InputGroup>
+            <Form.Control as="textarea" aria-label="With textarea" />
+          </InputGroup>
+        </div>
+      </Collapse>
+    </>
+  );
+};
