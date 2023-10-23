7141082aa7adb80fecf73f614eb98a336ba9be80
AbubakarMian <41990476+AbubakarMian@users.noreply.github.com>
2023-10-23 21:37:11 +0500

    fixes for UI

--- src/common/Constants.js
+++ src/common/Constants.js
@@ -1,5 +1,5 @@
-const base_url = 'http://localhost/demas_backend/public/api';
-// const base_url = "https://demastransport.com/demas_backend/public/api";
+// const base_url = 'http://localhost/demas_backend/public/api';
+const base_url = "https://demastransport.com/demas_backend/public/api";
 
 export const Constant = {
   basic_token: "Basic ZGVtYXMtYXBwLW1vYmlsZTtaR1Z0WVhNdFlYQndMVzF2WW1sc1pRPT0=",
